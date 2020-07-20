cd63929e28b148e604360d38240a741d96d28127
Frank Y <franklin_yegon@yahoo.com>
2020-07-20 12:55:27 +0300

    final javascript

--- diform/script.js
+++ diform/script.js
@@ -1,50 +1,42 @@
+function output(e){
+event.preventDefault();
 
-var DD= parseInt(prompt("enter date DD (DDmmccyy)"));
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-if (DD<1){alert("invalid date")}
-else if (DD<=31) {
-var MM= parseInt(prompt("enter month MM (ddMMccyy)"));
-  if (MM<1){alert("invalid month")}
-  else if (MM>12){
-  alert("invalid month");}else{
-  var CC= parseInt(prompt("enter century CC (ddmmCCyy)"));
-  if (CC<19){
-  alert("invalid century")}
-  else if (CC>20){
-  alert("future centure. Please enter again");
-  }
-  else {var YY= parseInt(prompt("enter year YY (ddmmccYY)"));
-  }
-  }
-  }
-else {alert("invalid date of birth");
-}
+var Date = parseInt(document.getElementById("DD").value);
+var Month = parseInt(document.getElementById("MM").value);
+//const Cent = parseInt(document.getElementById("CC").value)
+var Year = document.getElementById("YY").value;
+var sex = document.getElementById("gender").value;
 
-var dayWeek =parseInt((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7);
+var Cent = parseInt(Year.slice(0,2));
+var Yeaar = parseInt(Year.slice(2,4));
+
+var dayWeek =Math.floor(((((Cent/4)-2*Cent-1)+((5*Yeaar/4))+((26*(Month+1)/10))+Date)%7));
 //alert(dayWeek);
- 
-var gender= prompt("enter your gender");
-if (gender=="male"){
-if (dayWeek==0){consol.log("Your Ghanaian name is Kwasi");}
- else if (dayWeek==1){alert("Your Ghanaian name is Kwadwo");}
- else if (dayWeek==2){alert("Your Ghanaian name is Kwabena");}
- else if (dayWeek==3){alert("Your Ghanaian name is Kwaku");}
- else if (dayWeek==4){alert("Your Ghanaian name is Yaw");}
- else if (dayWeek==5){alert("Your Ghanaian name is Kofi");}
- else if (dayWeek==6){alert("Your Ghanaian name is Kwame");}
- else {alert("invalid")}
+if (sex=="male"){
+  if (dayWeek==0){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[0]+". "+"Your Ghanaian name is "+maleNames[0]);}
+   else if (dayWeek==1){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[1]+". "+"Your Ghanaian name is "+maleNames[1]);}
+   else if (dayWeek==2){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[2]+". "+"Your Ghanaian name is "+maleNames[2]);}
+   else if (dayWeek==3){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[3]+". "+"Your Ghanaian name is "+maleNames[3]);}
+   else if (dayWeek==4){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[4]+". "+"Your Ghanaian name is "+maleNames[4]);}
+   else if (dayWeek==5){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[5]+". "+"Your Ghanaian name is "+maleNames[5]);}
+   else if (dayWeek==6){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[6]+". "+"Your Ghanaian name is "+maleNames[6]);}
+   else {document.getElementById("outpuuut").innerHTML=("invalid")}
+  }
+  else if (sex=="female"){
+  if (dayWeek==0){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[0]+". "+"Your Ghanaian name is "+femaleNames[0]);}
+   else if (dayWeek==1){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[1]+". "+"Your Ghanaian name is "+femaleNames[1]);}
+   else if (dayWeek==2){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[2]+". "+"Your Ghanaian name is "+femaleNames[2]);}
+   else if (dayWeek==3){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[3]+". "+"Your Ghanaian name is "+femaleNames[3]);}
+   else if (dayWeek==4){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[4]+". "+"Your Ghanaian name is "+femaleNames[4]);}
+   else if (dayWeek==5){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[5]+". "+"Your Ghanaian name is "+femaleNames[5]);}
+   else if (dayWeek==6){document.getElementById("outpuuut").innerHTML=("You were born on "+dayNames[6]+". "+"Your Ghanaian name is "+femaleNames[6]);}
+   else {document.getElementById("outpuuut").innerHTML=("invalid")}
+  }
+  else {document.getElementById("outpuuut").innerHTML=("invalid gender");}
+  
+  
 }
-else if (gender=="female"){
-if (dayWeek==0){alert("Your Ghanaian name is Akosua");}
- else if (dayWeek==1){alert("Your Ghanaian name is Adwoa");}
- else if (dayWeek==2){alert("Your Ghanaian name is Abenaa");}
- else if (dayWeek==3){alert("Your Ghanaian name is Akua");}
- else if (dayWeek==4){alert("Your Ghanaian name is Yaa");}
- else if (dayWeek==5){alert("Your Ghanaian name is Afua");}
- else if (dayWeek==6){alert("Your Ghanaian name is Ama");}
- else {alert("invalid")}
-}
-else {alert("invalid gender");}
-
-    
-
