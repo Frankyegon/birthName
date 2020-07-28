0e37bb1f470bf2b3396797c50d40cdcc6c6e9807
Frank Y <franklin_yegon@yahoo.com>
2020-07-28 18:03:29 +0300

    ip week 2 javascript

--- ipbirthName.js
+++ ipbirthName.js
@@ -1,46 +1,61 @@
-var DD= parseInt(prompt("enter date"));
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-if (DD<=31) {
-var MM= parseInt(prompt("enter month"));
-	if (MM>12){
-  alert("invalid month");}else{
-  var CC= parseInt(prompt("enter cc"));
-  if (CC<19){
-  alert("extinct")}
-  else if (CC>20){
-  alert("future");
-  }
-  else {var YY= parseInt(prompt("enter YY"));
-  } 
-  }
-  }
-else {alert("invalid date");
+
+function input1(){
+DD = parseInt(document.getElementById("DD").value);
+MM = parseInt(document.getElementById("MM").value)
+CC = parseInt(document.getElementById("CC").value)
+YY = parseInt(document.getElementById("YY").value)
+gender = document.getElementById("gender").value
+
+alert(DD+MM+CC+YY);
+
+//var year = [DD,MM,CC,YY];
+
+
+if(DD<1){alert("enter date higher than 1")}
+if (DD){alert("Enter date less than 31")}
+if (MM<1){alert("Enter month more or equal to 1")}
+if (MM>12){alert("Enter month less or equal to 12");}
+if (CC<19){alert("Enter CC value 19/20")}
+if (CC>20){alert("Enter CC value 19/20")}
+
+  else{alert ("code reach here")}
 }
+  
 
-var dayWeek =parseInt((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7);
-//alert(dayWeek);
+dayWeek =parseInt((((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7);
+alert(dayWeek);
  
-var gender= prompt("enter your gender");
+function Gender1() {
+
+
 if (gender=="male"){
-if (dayWeek==0){consol.log("Kwasi");}
- else if (dayWeek==1){alert("Kwadwo");}
- else if (dayWeek==2){alert("Kwabena");}
- else if (dayWeek==3){alert("Kwaku");}
- else if (dayWeek==4){alert("Yaw");}
- else if (dayWeek==5){alert("Kofi");}
- else if (dayWeek==6){alert("Kwame");}
+if (dayWeek==0){alert("You were born on "+dayNames[0]+". "+"Your Ghanaian name is "+maleNames[0]);}
+ else if (dayWeek==1){alert("You were born on "+dayNames[1]+". "+"Your Ghanaian name is "+maleNames[1]);}
+ else if (dayWeek==2){alert("You were born on "+dayNames[2]+". "+"Your Ghanaian name is "+maleNames[2]);}
+ else if (dayWeek==3){alert("You were born on "+dayNames[3]+". "+"Your Ghanaian name is "+maleNames[3]);}
+ else if (dayWeek==4){alert("You were born on "+dayNames[4]+". "+"Your Ghanaian name is "+maleNames[4]);}
+ else if (dayWeek==5){alert("You were born on "+dayNames[5]+". "+"Your Ghanaian name is "+maleNames[5]);}
+ else if (dayWeek==6){alert("You were born on "+dayNames[6]+". "+"Your Ghanaian name is "+maleNames[6]);}
  else {alert("invalid")}
 }
 else if (gender=="female"){
-if (dayWeek==0){alert("Akosua");}
- else if (dayWeek==1){alert("Adwoa");}
- else if (dayWeek==2){alert("Abenaa");}
- else if (dayWeek==3){alert("Akua");}
- else if (dayWeek==4){alert("Yaa");}
- else if (dayWeek==5){alert("Afua");}
- else if (dayWeek==6){alert("Ama");}
+if (dayWeek==0){alert("You were born on "+dayNames[0]+". "+"Your Ghanaian name is "+femaleNames[0]);}
+ else if (dayWeek==1){alert("You were born on "+dayNames[1]+". "+"Your Ghanaian name is "+femaleNames[1]);}
+ else if (dayWeek==2){alert("You were born on "+dayNames[2]+". "+"Your Ghanaian name is "+femaleNames[2]);}
+ else if (dayWeek==3){alert("You were born on "+dayNames[3]+". "+"Your Ghanaian name is "+femaleNames[3]);}
+ else if (dayWeek==4){alert("You were born on "+dayNames[4]+". "+"Your Ghanaian name is "+femaleNames[4]);}
+ else if (dayWeek==5){alert("You were born on "+dayNames[5]+". "+"Your Ghanaian name is "+femaleNames[5]);}
+ else if (dayWeek==6){alert("You were born on "+dayNames[6]+". "+"Your Ghanaian name is "+femaleNames[6]);}
  else {alert("invalid")}
 }
 else {alert("invalid gender");}
 
+}
 
+function findName(){
+  getGender1();
+}
